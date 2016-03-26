52319adf7da421064d6d1ca3b15b2c81293b3c77
osama39 <osamakhaled2012@gmail.com>
2016-03-26 03:26:43 +0200

    Exported jar problem

--- djuvpp-djvureader-windows-f4eb608c437c/DjVu++ viewer module/src/com/lizardtech/djview/Main.java
+++ djuvpp-djvureader-windows-f4eb608c437c/DjVu++ viewer module/src/com/lizardtech/djview/Main.java
@@ -1,12 +1,15 @@
 package com.lizardtech.djview;
 
 import com.lizardtech.djview.frame.Frame;
+import com.lizardtech.djvu.DjVuOptions;
+
 import java.awt.FileDialog;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.swing.JFrame;
 
 /*
@@ -21,51 +24,54 @@ import javax.swing.JFrame;
  */
 public class Main extends JFrame {
 
-    /**
-     * Creates new form Main
-     */
-    public Main() {
-        try {
-            opendialog();
-        } catch (IOException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	/**
+	 * Creates new form Main
+	 */
+	public Main() {
+//		DjVuOptions.err = System.err;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(() -> {
-            new Main();
-        });
-    }
+		try {
+			opendialog();
+		} catch (IOException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-    private void opendialog() throws IOException {
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(() -> {
+			new Main();
+		});
+	}
 
-        FileDialog fd = new FileDialog(this, "open djvu file", FileDialog.LOAD);
-        fd.setMultipleMode(true);
-        fd.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/djvuNewIcon.png")));
-        fd.setVisible(true);
-        if (fd.getDirectory() != null) {
-            File files[] = fd.getFiles();
-            for (File file : files) {
-                String url;
-                url = "" + file.toURI().toURL();
-                url = url.substring(5, url.length());
-                String name = file.getName();
-                openBookInNewTab(url, name);
-            }
+	private void opendialog() throws IOException {
 
-        }
+		FileDialog fd = new FileDialog(this, "open djvu file", FileDialog.LOAD);
+		fd.setMultipleMode(true);
+		fd.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/djvuNewIcon.png")));
+		fd.setVisible(true);
+		if (fd.getDirectory() != null) {
+			File files[] = fd.getFiles();
+			for (File file : files) {
+				String url;
+				url = "" + file.toURI().toURL();
+				url = url.substring(5, url.length());
+				String name = file.getName();
+				openBookInNewTab(url, name);
+			}
 
-    }
+		}
 
-    public void openBookInNewTab(final String url, String name) {
-        Frame f = new Frame(url);
-        f.setVisible(true);
-        f.setSize(700, 900);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	}
+
+	public void openBookInNewTab(final String url, String name) {
+		Frame f = new Frame(url);
+		f.setVisible(true);
+		f.setSize(700, 900);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
