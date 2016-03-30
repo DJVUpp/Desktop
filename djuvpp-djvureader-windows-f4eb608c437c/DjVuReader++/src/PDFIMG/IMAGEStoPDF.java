2103b33eabbb3ece02f2ab16cf486c7fcece4461
Abdelrhman Mohamed <aboooooooodm@gmail.com>
2016-03-30 02:26:37 +0200

    Update IMAGEStoPDF.java

--- djuvpp-djvureader-windows-f4eb608c437c/DjVuReader++/src/PDFIMG/IMAGEStoPDF.java
+++ djuvpp-djvureader-windows-f4eb608c437c/DjVuReader++/src/PDFIMG/IMAGEStoPDF.java
@@ -16,11 +16,22 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.FileNotFoundException;
 import javax.swing.JOptionPane;
 /**
- *
+ * Convert Image To PDF Updated By Abdelrhman Mohamed 
  * @author FATHI HOSSAM
  */
 public class IMAGEStoPDF {
-   public static void convertToIMG(String[] RESOURCES,String result,String path,int s) throws FileNotFoundException, DocumentException, BadElementException, IOException {
+   /**
+    * Convert Images To Pdf
+    * @param RESOURCES
+    * @param result
+    * @param path
+    * @param s
+    * @throws FileNotFoundException
+    * @throws DocumentException
+    * @throws BadElementException
+    * @throws IOException 
+    */
+    public static void convertToIMG(String[] RESOURCES,String result,String path,int s) throws FileNotFoundException, DocumentException, BadElementException, IOException {
 
         // step 1
         Document document = new Document();
@@ -33,7 +44,12 @@ public class IMAGEStoPDF {
         Image img;
        for (int i = 0; i < RESOURCES.length; i++) {
             img = Image.getInstance(String.format(path, RESOURCES[i]));
-            
+            //if biggger than 800*700 scale it to be fit with page
+            if(img.getScaledHeight()>800 &&img.getScaledWidth()>700)
+            img.scaleAbsolute(document.getPageSize().getWidth()-50,document.getPageSize().getHeight()-50);
+            else{
+            img.setAbsolutePosition(document.getPageSize().getWidth()/5, document.getPageSize().getHeight()/2);
+            }
             document.add(img);
         }
         // step 5
@@ -41,22 +57,7 @@ public class IMAGEStoPDF {
                      
         if(s==1){
                JOptionPane.showMessageDialog(null,"Converted Successfully");
-            //JOptionPane.showMessageDialog(null, "Finished\nFile save in :\nC:\\DjVu++Task\\ImagestoPDF\\"+RESOURCES[0].substring(0,RESOURCES[0].lastIndexOf("."))+".pdf");
         }
     }
-//    public static void main(String[] args) throws IOException, FileNotFoundException, DocumentException {
-//        File [] img = new File[4];
-//        img[0]=new File("D:\\FILMEY\\Downloads\\Image\\1.jpg");
-//        img[1]=new File("D:\\FILMEY\\Downloads\\Image\\12.jpg");
-//        img[2]=new File("D:\\FILMEY\\Downloads\\Image\\23.jpg");
-//        img[3]=new File("D:\\FILMEY\\Downloads\\Image\\13.jpg");
-//        String [] name = new String[img.length];
-//        for(int i = 0;i<img.length;i++){
-//            name[i]=img[i].getName();
-//        }
-//        String path =img[0].getPath();
-//        System.out.println(path);
-//        convertToIMG(name,"D:\\fa.pdf","D:\\FILMEY\\Downloads\\Image\\%s");
-//    }
     
 }
